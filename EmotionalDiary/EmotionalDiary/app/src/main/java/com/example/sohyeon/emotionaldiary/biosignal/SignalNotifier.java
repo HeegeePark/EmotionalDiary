b34fa5b35296eb223f4912d8159aa364a6386722
HeegeePark <phg980513@naver.com>
2020-03-16 00:13:28 +0900

    reset

--- EmotionalDiary/EmotionalDiary/app/src/main/java/com/example/sohyeon/emotionaldiary/biosignal/SignalNotifier.java
+++
@@ -1,9 +0,0 @@
-package com.example.sohyeon.emotionaldiary.biosignal;
-
-/**
- * Created by lhw48 on 2016-06-22.
- */
-public interface SignalNotifier {
-    public void didReadPpg(int ppg);
-    public void didReadHrv(double[] hrv);
-}
